93bf633e5a81a2100e6162d03d53bbc89f596d07
AndresQuinto5 <qui18288@uvg.edu.gt>
2023-03-07 16:27:59 -0600

    fps counter

--- screensaver.cpp
+++ screensaver.cpp
@@ -1,14 +1,36 @@
 #include <SDL2/SDL.h>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
+#include <SDL2/SDL_ttf.h>
+
+
 
 int main(int argc, char* argv[]) {
+        // Initialize TTF
+    if (TTF_Init() == -1) {
+        std::cerr << "Error initializing TTF: " << TTF_GetError() << std::endl;
+        return 1;
+    }
+
+    // Create font object
+    TTF_Font* font = TTF_OpenFont("arial.ttf", 24);
+    if (!font) {
+        std::cerr << "Error loading font: " << TTF_GetError() << std::endl;
+        return 1;
+    }
+
     // Initialize SDL
     SDL_Init(SDL_INIT_VIDEO);
 
     // Create a window and renderer
     SDL_Window* window = SDL_CreateWindow("Screensaver", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    // Initialize variables for FPS counter
+    Uint32 start_time = SDL_GetTicks();
+    int frame_count = 0;
+    int fps = 0;
 
     // Generate random colors
     srand(time(nullptr));
@@ -26,17 +48,54 @@ int main(int argc, char* argv[]) {
     // Run the event loop
     bool quit = false;
     while (!quit) {
+        // Calculate FPS
+        Uint32 current_time = SDL_GetTicks();
+        if (current_time - start_time >= 1000) {
+            fps = frame_count;
+            frame_count = 0;
+            start_time = current_time;
+            std::cout << "FPS: " << fps << std::endl;
+        }
+
+        // Handle events
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 quit = true;
             }
         }
+
+        // Draw random color to screen
+        r = rand() % 256;
+        g = rand() % 256;
+        b = rand() % 256;
+        SDL_SetRenderDrawColor(renderer, r, g, b, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+
+        // Draw FPS to top-left corner of screen
+        SDL_Color textColor = {255, 255, 255};
+        SDL_Surface* textSurface = TTF_RenderText_Solid(font, ("FPS: " + std::to_string(fps)).c_str(), textColor);
+        SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+        SDL_Rect textRect = {0, 0, textSurface->w, textSurface->h};
+        SDL_RenderCopy(renderer, textTexture, nullptr, &textRect);
+
+        // Add a 5-second delay
+        SDL_Delay(5000);
+
+        // Update the screen
+        SDL_RenderPresent(renderer);
+
+        // Increment frame count
+        frame_count++;
     }
 
     // Clean up
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+        // Clean up font and TTF
+    TTF_CloseFont(font);
+    TTF_Quit();
+
     return 0;
 }
