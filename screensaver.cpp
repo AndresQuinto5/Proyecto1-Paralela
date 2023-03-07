da149cbaaf3a73b9331d2b1993fc99e3df714c51
AndresQuinto5 <qui18288@uvg.edu.gt>
2023-03-07 17:48:33 -0600

    El counter y las figuras hacen efecto de rainbow en lugar del fondo

--- screensaver.cpp
+++ screensaver.cpp
@@ -3,6 +3,11 @@
 #include <ctime>
 #include <iostream>
 #include <SDL2/SDL_ttf.h>
+#include <cmath>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int BALL_RADIUS = 10;
 
 int main(int argc, char* argv[]) {
     // Initialize TTF
@@ -22,7 +27,7 @@ int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
 
     // Create a window and renderer
-    SDL_Window* window = SDL_CreateWindow("Screensaver", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("Screensaver", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     // Initialize variables for FPS counter
@@ -44,8 +49,8 @@ int main(int argc, char* argv[]) {
     // Initialize the positions and velocities of each ball
     srand(time(NULL));
     for (int i = 0; i < num_balls; i++) {
-        balls[i].x = rand() % (640 - 20) + 20;
-        balls[i].y = rand() % (480 - 20) + 20;
+        balls[i].x = rand() % (SCREEN_WIDTH - BALL_RADIUS*2) + BALL_RADIUS;
+        balls[i].y = rand() % (SCREEN_HEIGHT - BALL_RADIUS*2) + BALL_RADIUS;
         balls[i].dx = rand() % 10 - 5;
         balls[i].dy = rand() % 10 - 5;
     }
@@ -76,60 +81,59 @@ int main(int argc, char* argv[]) {
         // Calculate rainbow color values using time elapsed
         Uint8 r = (1 + sin(time_elapsed * 0.5f)) * 128;
         Uint8 g = (1 + sin(time_elapsed * 0.7f)) * 128;
-        Uint8 b = (1 + sin(time_elapsed * 0.9f)) * 128;
+        Uint8 b = (1 + sin(time_elapsed * 1.3f)) * 128;
 
-        // Set background color to rainbow color
-        SDL_SetRenderDrawColor(renderer, r, g, b, SDL_ALPHA_OPAQUE);
+        // Set background color
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        // Update ball positions and draw them
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255,SDL_ALPHA_OPAQUE);
+        // Render balls
+        SDL_SetRenderDrawColor(renderer, r, g, b, 255);
         for (int i = 0; i < num_balls; i++) {
-        // Update ball position
-        balls[i].x += balls[i].dx;
-        balls[i].y += balls[i].dy;
-                // Handle ball-wall collisions
-        if (balls[i].x < 0 || balls[i].x > 640 - 20) {
-            balls[i].dx *= -1;
+            // Update position based on velocity
+            balls[i].x += balls[i].dx;
+            balls[i].y += balls[i].dy;
+
+            // Handle collisions with walls
+            if (balls[i].x < BALL_RADIUS || balls[i].x > SCREEN_WIDTH - BALL_RADIUS) {
+                balls[i].dx = -balls[i].dx;
+            }
+            if (balls[i].y < BALL_RADIUS || balls[i].y > SCREEN_HEIGHT - BALL_RADIUS) {
+                balls[i].dy = -balls[i].dy;
+            }
+
+            // Draw ball
+            SDL_Rect ball_rect = { balls[i].x - BALL_RADIUS, balls[i].y - BALL_RADIUS, BALL_RADIUS * 2, BALL_RADIUS * 2 };
+            SDL_RenderFillRect(renderer, &ball_rect);
         }
-        if (balls[i].y < 0 || balls[i].y > 480 - 20) {
-            balls[i].dy *= -1;
+
+        // Render FPS counter
+        if (font) {
+            std::string fps_str = "FPS: " + std::to_string(fps);
+            SDL_Color font_color = { r, g, b, 255 };
+            SDL_Surface* text_surface = TTF_RenderText_Solid(font, fps_str.c_str(), font_color);
+            if (text_surface) {
+                SDL_Texture* text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
+                SDL_Rect text_rect = { 10, 10, text_surface->w, text_surface->h };
+                SDL_RenderCopy(renderer, text_texture, NULL, &text_rect);
+                SDL_FreeSurface(text_surface);
+                SDL_DestroyTexture(text_texture);
+            }
         }
 
-        // Draw ball
-        SDL_Rect ball_rect = { balls[i].x, balls[i].y, 20, 20 };
-        SDL_RenderFillRect(renderer, &ball_rect);
-    }
-
-        // Draw FPS counter
-        SDL_Color text_color = { 255, 255, 255, SDL_ALPHA_OPAQUE };
-        std::string fps_text = "FPS: " + std::to_string(fps);
-        SDL_Surface* text_surface = TTF_RenderText_Solid(font, fps_text.c_str(), text_color);
-        SDL_Texture* text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
-        SDL_Rect text_rect = { 10, 10, text_surface->w, text_surface->h };
-        SDL_RenderCopy(renderer, text_texture, NULL, &text_rect);
-        SDL_FreeSurface(text_surface);
-        SDL_DestroyTexture(text_texture);
-
-        // Update the screen
+        // Update screen
         SDL_RenderPresent(renderer);
 
-        // Increase frame count
+        // Increment frame count
         frame_count++;
-
-        // Delay to achieve desired FPS
-        Uint32 end_time = SDL_GetTicks();
-        Uint32 frame_time = end_time - current_time;
-        if (frame_time < 1000 / 60) {
-            SDL_Delay(1000 / 60 - frame_time);
-        }
     }
 
-    // Clean up resources and quit
-    TTF_CloseFont(font);
-    TTF_Quit();
+    // Clean up
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    TTF_CloseFont(font);
+    TTF_Quit();
     SDL_Quit();
+
     return 0;
 }
